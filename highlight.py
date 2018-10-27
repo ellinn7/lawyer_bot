056f92c1b5a75f1f3bf0718c584cd4f064569ff9
Вятчанин Алексей (Vyatchanin_AS) <Vyatchanin_AS@directum.ru>
2018-10-27 11:31:53 +0400

    Метод highlight() сохраняет обработанные изображения и возвращает список путей до них

--- highlight.py
+++ highlight.py
@@ -1,13 +1,12 @@
 import sys
+import os
 from PIL import Image, ImageDraw
 
 #im = Image.open("out1-1.jpg")
-
 #draw = ImageDraw.Draw(im, 'RGBA')
 #draw.rectangle([100, 20, 500, 300], fill = (50, 50, 50, 64))
 #draw.line((0, im.size[1], im.size[0], 0), fill=128)
 #del draw
-
 #im.show()
 
 class StartingCoordinate:
@@ -33,31 +32,46 @@ class Highlight:
     self.starting_coordinate = starting_coordinate
     self.rectangle_dimensions = rectangle_dimensions
     self.color = color
-    
-def highlight(images, highlights):
-#
-  print("start highlight")
 
-  for i in range(0, len(images)):
-    image = Image.open(images[i])
+def generateOverlayFileName(full_path):
+#
+  base_name = os.path.basename(full_path)
+  name = os.path.splitext(base_name)[0]
+  return full_path.replace(name, name + "_overlay")
+#
+
+def highlight(image_paths, highlights):
+#
+  overlay_images = []
+
+  for i in range(0, len(image_paths)):
+    image = Image.open(image_paths[i])
 
     draw = ImageDraw.Draw(image, 'RGBA')
-    #draw.rectangle([100, 20, 500, 300], fill = (50, 50, 50, 64))
-    #TODO: учесть page_number
-    draw.rectangle([highlights[i].starting_coordinate.x, highlights[i].starting_coordinate.y, highlights[i].rectangle_dimensions.width, highlights[i].rectangle_dimensions.height],
-                   fill = (highlights[i].color.r, highlights[i].color.g, highlights[i].color.b, highlights[i].color.a))
-    del draw
-    image.show()
+    draw.rectangle([highlights[i].starting_coordinate.x,
+                    highlights[i].starting_coordinate.y,
+                    highlights[i].rectangle_dimensions.width,
+                    highlights[i].rectangle_dimensions.height],
+                    fill = (highlights[i].color.r,
+                            highlights[i].color.g,
+                            highlights[i].color.b,
+                            highlights[i].color.a))
 
-  print("stop highlight")
+    new_name = generateOverlayFileName(image_paths[i])
+    image.save(new_name)
+    overlay_images.append(new_name)
+
+  del draw
+  return overlay_images
 #
-grey = Color(50, 50, 50, 64)
 
-images = ["d:/hack/test/image1.jpg",
-          "d:/hack/test/image2.jpg"]
-#highlights = [[page_num, (x, y), (width, height), color], ...]
-#highlights = [[0, (100, 200), (250, 50), "color1"], [1, (40, 80), (95, 160), "color2"]]
-highlights = [Highlight(0, StartingCoordinate(100, 200), RectangleDimensions(250, 50), grey),
-              Highlight(1, StartingCoordinate(40, 80), RectangleDimensions(95, 160), grey)]
+#grey = Color(50, 50, 50, 64)
+#images = ["d:/hack/test/image1.jpg",
+#          "d:/hack/test/image2.jpg"]
+#highlights = [Highlight(0, StartingCoordinate(100, 200), RectangleDimensions(250, 50), grey),
+#              Highlight(1, StartingCoordinate(40, 80), RectangleDimensions(95, 160), grey)]
+#
+#overlay_images = highlight(images, highlights)
 
-highlight(images, highlights)
+#for im in overlay_images:
+#  print(im)
